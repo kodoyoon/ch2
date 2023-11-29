24288adbde51b53ea75b773465ec1cefaacee128
doyoon <doyoun0527@naver.com>
2023-11-29 15:22:06 +0900

    원격 프로그램의 작성과 실행

--- src/main/java/com/fastcampus/ch2/Main.java
+++ src/main/java/com/fastcampus/ch2/Main.java
@@ -1,10 +1,6 @@
 package com.fastcampus.ch2;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+
 
 public class Main {
   public static void main(String[] args) {
